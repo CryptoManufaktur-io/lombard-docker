a9a98372fa76f0e6ad2dde06974825c4507b7c04
Antonio Savage <antonio.savage@galaxy.com>
2025-11-10 11:35:49 -0500

    update starknet sepolia rpc endpoint for testnet

--- notaryd/docker-entrypoint.sh
+++ notaryd/docker-entrypoint.sh
@@ -143,7 +143,7 @@ else
   dasel put -f /cosmos/config/app.toml -v 64 evm.sepolia.required_confirmations
   dasel put -f /cosmos/config/app.toml -v true -t bool evm.sepolia.enabled
 
-  dasel put -f /cosmos/config/app.toml -v "https://starknet-sepolia.public.blastapi.io" starknet.sepolia.rpc_url
+  dasel put -f /cosmos/config/app.toml -v "https://rpc.starknet-testnet.lava.build:443" starknet.sepolia.rpc_url
   dasel put -f /cosmos/config/app.toml -v "SN_SEPOLIA" starknet.sepolia.chain_id
   dasel put -f /cosmos/config/app.toml -v "10s" starknet.sepolia.timeout
 
